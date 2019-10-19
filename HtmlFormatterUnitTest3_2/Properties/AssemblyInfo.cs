fa0e000392861faa0ffdf719cec76d057dff6238
Prangwalai994 <prangwalai.li@rmuti.ac.th>
2019-10-19 17:10:47 +0700

    3_2 HtmlFormatter (2)

---
+++ HtmlFormatterUnitTest3_2/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("HtmlFormatterUnitTest3_2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HtmlFormatterUnitTest3_2")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("fc36eeea-2701-4b17-9868-937dcbfb7d0f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ HtmlFormatterUnitTest3_2/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using HtmlFormatterUnitTest3_2;
+
+namespace HtmlFormatterUnitTest3_2
+{
+    [TestFixture]
+    public class HtmlFormatterTests
+    {
+        [Test]
+        public void FormatAsBold_WhenCalled_ShouldEncloseTheStringWithStrongElement()
+        {
+            var formatter = new HtmlFormatter();
+
+            var result = formatter.FormatAsBold("abc");
+
+            //Specific
+ //           Assert.That(result, Is.EqualTo("<strong>abc</strong>"));
+
+            //Mor general
+            Assert.That(result, Does.StartWith("<strong>"));
+        }
+    }
+}
